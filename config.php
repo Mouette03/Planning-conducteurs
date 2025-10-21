218fbe37d0aa6fc3bf1065002a34b0d997481a29
Mouette03 <mouette03@yahoo.fr>
2025-10-21 23:57:09 +0200

    update

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_HOST', 'mysql-fontainejeannette.alwaysdata.net');
+define('DB_NAME', 'fontainejeannette_bb');
+define('DB_USER', '415859');
+define('DB_PASS', 'La.mouette4694');
+define('DB_PREFIX', 'plan_');
+define('DB_CHARSET', 'utf8mb4');
+define('APP_DEBUG', true);
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ini_set('default_charset', 'UTF-8');
 if (!file_exists('config.php')) {
     header('Location: install.php');
