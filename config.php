093a2bf31d2fea89c091c4999fbed2a04909010a
Damien <mouette03@yahoo.fr>
2025-10-22 15:36:58 +0200

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_HOST', '');
-define('DB_NAME', '');
-define('DB_USER', '');
-define('DB_PASS', '');
-define('DB_PREFIX', 'plan_');
-define('DB_CHARSET', 'utf8mb4');
-define('APP_DEBUG', true);
